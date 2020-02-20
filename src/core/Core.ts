f9a45a54b163698c045d66a23ef90f9cd5b2c20d
Philipp Fehr <philipp.fehr@ambit-group.com>
2020-02-20 21:14:00 +0100

    fix cod

--- src/core/Core.ts
+++ src/core/Core.ts
@@ -2,6 +2,7 @@ import { Parser } from './Parser';
 import { Outputter } from './Outputter';
 import { BookDuplicateCounter } from './BookDuplicateCounter';
 import { FilterService } from './FilterService';
+import { LibraryStatistics } from './LibraryStatistics';
 
 export class Core {
   constructor(private input: string) {}
@@ -10,8 +11,9 @@ export class Core {
     const parser = new Parser(this.input);
     const context = parser.parse();
     const duplicates = BookDuplicateCounter.bookDuplicates(context);
+    const signupAverage = LibraryStatistics.averageSignUpTime(context);
     const libs = context.libraries.map(library =>
-      library.calculateScore(duplicates)
+      library.calculateScore(duplicates, signupAverage)
     );
     let sorted = libs.sort((a, b) => b.score - a.score);
     sorted = FilterService.filterDuplicates(sorted);
