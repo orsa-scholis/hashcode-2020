b261b209479cc055ac5f3cdea2e26a6a26683ad1
Philipp Fehr <philipp.fehr@ambit-group.com>
2020-02-20 20:05:23 +0100

    Add BookDuplicateCounter

---
+++ src/core/BookDuplicateCounter.ts
@@ -0,0 +1,18 @@
+import { Context } from "../models/Context";
+import { Book } from "../models/Book";
+
+export class BookDuplicateCounter{
+
+    public static bookDuplicates(context: Context): Map<number, number>
+    {
+        let duplicates = new Map;
+
+        context.books.forEach((book) => {
+            let libs = context.libraries.filter(lib => lib.books.includes(book));
+      
+            duplicates.set(book.id, libs.length);
+          });
+
+          return duplicates;
+    }
+}
